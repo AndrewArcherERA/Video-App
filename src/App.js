6215d348b9af3f23a996904f218cf6159ea38660
AndrewArcherERA <a.archer@erasolutions.us>
2024-05-17 17:33:26 -0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
 
 function App() {
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
 
     const getMovies = async () => {
         try {
             const response = await api.get("/api/v1/movies");
-
-            setMovies(response);
+            setMovies(response.data);
         } catch (err) {
             console.log(err);
         }
@@ -19,7 +23,17 @@ function App() {
         getMovies();
     }, []);
 
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/' element={<Home movies={movies} />} />
+                    <Route path='/Trailer/:ytTrailerId' element={<Trailer />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
